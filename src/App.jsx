bf50f4250975fb7ec6e658d55214ae6b280c89a5
Oleksandr Kalita <olexadr.kalita@gmail.com>
2023-11-12 23:14:12 +0100

    create redux store

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,17 @@
+import { useGetUsersQuery } from "./redux/usersApi";
 
 function App() {
+  const {data = {}, isLoading} = useGetUsersQuery();
+
+  if (isLoading) return <h1>Loading...</h1>
   return (
     <div className="wrapper">
       <h1>Main Page</h1>
+      <ul>
+        { 
+          data.map(item => <li key={item.id}>{item.name}</li>)
+        }
+      </ul>
     </div>
   );
 }
